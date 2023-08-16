6ef2ddbde62fcf5eb4c8bbfa82105c2ce2f8669d
priyanka-mehta02 <122629880+priyanka-mehta02@users.noreply.github.com>
2023-08-16 11:39:12 +0200

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-            <h1> this is the Home  </h1>
+            <h1> this is the Home for render  </h1>
            
     </div>
   )
